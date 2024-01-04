2239416440993448f7b9a56cc3a709ea9a007457
ahmad.abdullah <ahmad.abdullah@techloyce>
2024-01-04 13:27:53 +0500

    initial commit

---
+++ client/src/setUpProxy.js
@@ -0,0 +1,11 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//   app.use(
+//     '/your-custom-path', // Specify your custom path here
+//     createProxyMiddleware({
+//       target: 'http://localhost:3000', // Your local development server URL
+//       changeOrigin: true,
+//     })
+//   );
+// };
